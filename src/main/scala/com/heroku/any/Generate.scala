ec9573e8d8297d85e0de06336408c2a82c035e81
Ryan Brainard <ryan.brainard@gmail.com>
2013-05-12 00:43:45 -0700

    Constructor, getters, setters

--- src/main/scala/com/heroku/any/Generate.scala
+++ src/main/scala/com/heroku/any/Generate.scala
@@ -34,6 +34,7 @@ object Generate {
 
   def convertType(raw: String) = raw match {
     case "string"   => "String"
+    case "number"   => "Number"
     case "datetime" => "java.util.Date"
     case "uuid"     => "java.util.UUID"
     case other => other
@@ -43,24 +44,57 @@ object Generate {
     val out = new PrintWriter(new FileOutputStream(s"target/generated/com/heroku/api/${resourceName}.java"))
     val writer = new JavaWriter(out)
 
-    val java = writer.emitPackage("com.heroku")
+    // header
+    val resourceClass = writer
+      .emitPackage("com.heroku")
       .beginType(s"com.heroku.${resourceName.replace(" ", "")}", "class", PUBLIC | FINAL)
 
+    // fields
     resourceDetail.attributes.foreach { case (attributeName: String, attributeDetail: Attribute) =>
-      java.emitField(convertType(attributeDetail.`type`), attributeName, PRIVATE | FINAL)
+      resourceClass
+        .emitEmptyLine()
+        .emitJavadoc(TextUtils.capitalize(attributeDetail.description))
+        .emitField(convertType(attributeDetail.`type`), attributeName, PRIVATE | FINAL)
     }
 
-    java.emitEmptyLine()
+    resourceClass.emitEmptyLine()
 
+    // empty constructor
+    resourceClass
+      .emitJavadoc(s"Construct empty $resourceName")
+      .beginMethod(null, resourceName, PUBLIC)
+      .endMethod()
+      .emitEmptyLine()
+
+    // fully-qualified constructor
+    resourceClass.beginMethod(null, resourceName, PUBLIC, resourceDetail.attributes.map {
+      case (attributeName: String, attributeDetail: Attribute) =>
+        Seq(convertType(attributeDetail.`type`), TextUtils.camelCase(attributeName))
+    }.flatten.toSeq:_*)
+    resourceDetail.attributes.map { case (attributeName: String, attributeDetail: Attribute) =>
+      resourceClass.emitStatement(s"this.$attributeName = ${TextUtils.camelCase(attributeName)}")
+      Seq(convertType(attributeDetail.`type`), TextUtils.camelCase(attributeName))
+    }
+    resourceClass.endMethod().emitEmptyLine()
+
+    // getters and setters
     resourceDetail.attributes.foreach { case (attributeName: String, attributeDetail: Attribute) =>
-      java.beginMethod(convertType(attributeDetail.`type`), s"get${TextUtils.capitalCamelCase(attributeName)}", PUBLIC)
-        .emitStatement(s"return $attributeName")
+      val uType = convertType(attributeDetail.`type`)
+      val uField = TextUtils.capitalCamelCase(attributeName)
+      resourceClass
+        .emitJavadoc(s"Get ${attributeDetail.description}")
+        .beginMethod(uType, s"get$uField", PUBLIC)
+        .emitStatement(s"return this.$attributeName")
         .endMethod()
+        .emitEmptyLine()
+        .emitJavadoc(s"Set ${attributeDetail.description}")
+        .beginMethod(uType, s"set$uField", PROTECTED, uType, uField)
+        .emitStatement(s"this.$attributeName = $uField")
+        .endMethod()
+        .emitEmptyLine()
     }
 
-//      java.emitJavadoc(resourceDetail.description)
-
-    java.endType()
+    resourceClass.endType()
 
     out.flush()
     out.close()
