2262e9a7c8ef7895aa275d92db90632e0f510968
Ryan Brainard <ryan.brainard@gmail.com>
2013-05-21 00:29:20 -0700

    Add parent entity to rich schema classes

--- src/main/scala/com/heroku/any/schema/rich/RichSchema.scala
+++ src/main/scala/com/heroku/any/schema/rich/RichSchema.scala
@@ -7,16 +7,18 @@ case class Schema(filename: String,
   def resourcesSecondClass = resources.flatMap { resource =>
     resource.attributes.filter(_.name.contains(":")).map { attribute =>
       val Array (obj,field) = attribute.name.split(":")
-      (obj.capitalize, Attribute(field, field, attribute.dataType, attribute.serialized, ""))
+      (obj.capitalize, Attribute(null, field, field, attribute.dataType, attribute.serialized, ""))
     }
   }.foldLeft(Map[String, Set[Attribute]]()) { (agg,a) =>
     agg + (a._1 -> agg.get(a._1).map(_ + a._2).getOrElse(Set(a._2)))
   }.filterNot(r => resources.exists(resource => resource.modelClassName.raw == r._1)).map { r =>
-    Resource(r._1, Seq(), r._2.toSeq, "")
+    val rich = Resource(this, r._1, Seq(), r._2.toSeq, "")
+    rich.copy(attributes = rich.attributes.map(_.copy(resource = rich)))
   }
 }
 
-case class Resource(name: String,
+case class Resource(schema: Schema,
+                    name: String,
                     actions: Seq[Action],
                     attributes: Seq[Attribute],
                     description: String) {
@@ -33,7 +35,8 @@ case class Resource(name: String,
   def isModel = !attributes.headOption.exists(_.massaged.dataType.raw.startsWith("dictionary")) // TODO: de-dupe
 }
 
-case class Attribute(name: String,
+case class Attribute(resource: Resource,
+                     name: String,
                      description: String,
                      dataType: DataType,
                      serialized: Boolean,
@@ -43,17 +46,18 @@ case class Attribute(name: String,
   private val dictPattern = """^\"?\{([a-z-]+)\}\"?$""".r
   def massaged = if (name.contains(":")) {
     val Array (obj,field) = name.split(":")
-    Attribute(obj, obj, DataType(obj.capitalize), serialized, "")
+    Attribute(resource, obj, obj, DataType(obj.capitalize), serialized, "")
   } else {
     dictPattern.findFirstMatchIn(name).map { m =>
      val dictName = m.group(1) + dictPattern.findFirstMatchIn(example).map(_.group(1).capitalize).getOrElse("") + "Pairs"
      val valueDataType = dictPattern.findFirstMatchIn(example).map(_ => dataType.raw).getOrElse("string")
-      Attribute(dictName, description, DataType(s"dictionary[string,$valueDataType]"), serialized, example)
+      Attribute(resource, dictName, description, DataType(s"dictionary[string,$valueDataType]"), serialized, example)
     }.getOrElse(this)
   }
 }
 
-case class Action(name: String,
+case class Action(resource: Resource,
+                  name: String,
                   httpMethod: String,
                   path: String,
                   statuses: Seq[Int],
@@ -65,7 +69,7 @@ case class Action(name: String,
   private val pathVarPattern = """\{([a-z-]+)\}""".r
   def pathAttributes = {
     pathVarPattern.findAllIn(path).matchData.map { m =>
-      Attribute(TextUtils.camelCase(m.group(1)), m.group(0), DataType("string"), serialized = false, "")
+      Attribute(resource, TextUtils.camelCase(m.group(1)), m.group(0), DataType("string"), serialized = false, "")
     }
   }
   def requestEntity(resource: Resource) = {
